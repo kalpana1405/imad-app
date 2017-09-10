00c7c24f7f7a3fc79e259b8e525eba79b3e3acf2
kalpana1405 <30996110+kalpana1405@users.noreply.github.com>
2017-09-10 18:11:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,9 @@ var img= document.getElementById('sk');
 
 var marginLeft=0;
 function moveRight (){
-marginLeft= marginLeft + 10;
+marginLeft= marginLeft + 1;
 img.style.marginLeft = marginLeft + 'px' ;    
 }
 img.onclick= function (){
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,10000);
 }
