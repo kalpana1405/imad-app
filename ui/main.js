51b1542eb7adbe1608c2266c0680fb43ca3b62d0
kalpana1405 <30996110+kalpana1405@users.noreply.github.com>
2017-09-21 13:48:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,20 @@ img.onclick= function (){
     // its not working simultaneously with annimation 
     //document.write('told you');
 }
+
+//counter
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick= function(){
+    //make a request toi the counter endpoint
+    
+    //capture the response and store it in a variable
+    
+    //resend the variable in a correct span
+    counter = counter + 1 ; 
+    var span = document.getElementById('count');
+    span.innerHTML= counter.toString();
+    
+    
+};
+//
