6deae16d631e7b33a7243abc6cc1e41d8c119559
kalpana1405 <30996110+kalpana1405@users.noreply.github.com>
2017-09-21 14:39:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,3 @@
-console.log('Loaded!');
-
-//move the image!
-
-var img= document.getElementById('sk');
-
-var marginLeft=0;
-function moveRight (){
-marginLeft= marginLeft + 1;
-img.style.marginLeft = marginLeft + 'px' ;    
-}
-img.onclick= function (){
-    var interval = setInterval(moveRight,50);
-    
-    // its not working simultaneously with annimation 
-    //document.write('told you');
-}
 
 //counter
 var button = document.getElementById('counter');
