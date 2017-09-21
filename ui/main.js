3ad022fccf25a3af152254aebf610a067e359e70
kalpana1405 <30996110+kalpana1405@users.noreply.github.com>
2017-09-21 16:15:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,4 +31,53 @@ button.onclick= function(){
     
     
 };
-//
+
+// submit name
+
+var nameInput= document.getElementById('name');
+var name= nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick= function(){
+    // make a request to the server and send the name
+    
+    //capture a list of names and render it as a list 
+    var name= ['name1', 'name2', 'name3', 'name4'];
+    var list ='';
+    for (var i =0; i<name.length;i++){
+     list += '<li>' + name[i]+ '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
